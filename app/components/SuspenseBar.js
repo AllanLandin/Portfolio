ad640f0edfe5dac1d9f3e7bf470266778a391a3f
Allan <allan.landin25@gmail.com>
2024-07-23 19:40:58 -0300

    add border to suspensebar

--- app/components/SuspenseBar.js
+++ app/components/SuspenseBar.js
@@ -2,11 +2,10 @@ import Link from "next/link";
 import { GoProject } from "react-icons/go";
 import { SiAboutdotme } from "react-icons/si";
 import { IoIosContact } from "react-icons/io";
-import { Fade } from "react-awesome-reveal";
 
 export default function SuspenseBar() {
   return (
-    <nav className="flex gap-4 bg-secondary py-2 px-5 rounded-full fixed bottom-10 end-1/2 translate-x-1/2 text-primary text-3xl items-center justify-center">
+    <nav className="flex gap-4 bg-secondary py-2 px-5 rounded-full fixed bottom-10 end-1/2 translate-x-1/2 text-primary text-3xl items-center justify-center border-primary border-2">
       <Link className=" hover:text-emphasis" href="/">
         <SiAboutdotme />
       </Link>
