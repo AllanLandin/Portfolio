4dd7896150afc374c81f6664137b23a5eb93cae5
Allan <allan.landin25@gmail.com>
2024-07-25 21:23:40 -0300

    feat: take off the github readme from the projects list

--- app/utils/sortProjects.js
+++ app/utils/sortProjects.js
@@ -9,17 +9,22 @@ export default function sortProjects(projects) {
 }
 
 function rankProjects(projects) {
+  let githubReadmeIndex = projects.length;
   for (let i = 0; i < projects.length; i++) {
     let points = 0;
     let prj = projects[i];
 
     // points system
+    if (prj.name == "AllanLandin") githubReadmeIndex = i;
     if (prj.homepage) points += 3;
     if (prj.topics.includes("top")) points += 5;
+    if (prj.description) points += 2;
+
     points += prj.stargazers_count * 2;
     points += prj.watchers_count;
 
     projects[i].points = points;
   }
+  projects.splice(githubReadmeIndex, 1);
   return projects;
 }
