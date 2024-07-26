a0b389efa06b5140b72aa206644d83af20542afc
Allan <allan.landin25@gmail.com>
2024-07-26 12:47:45 -0300

    Change the way the not found image is loaded

--- app/not-found.js
+++ app/not-found.js
@@ -4,12 +4,12 @@ import Image from "next/image";
 export default function NotFound() {
   return (
     <div className="h-dvh w-vh bg-slate-300 flex flex-col items-center justify-center gap-3 bg-primary">
-      <Image
+      <img
         src="/images/notFound.png"
         height={2000 / 3}
         width={2000 / 3}
         alt="not found image"
-      ></Image>
+      ></img>
       <p className="text-3xl font-poppins">Página não encontrada!</p>
       <Link
         href="/"
